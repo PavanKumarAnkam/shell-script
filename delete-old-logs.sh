e83eec2e731e58c682aa41aaff090d79960ce6db
PavanKumarAnkam <pa1kumarankam@gmail.com>
2024-08-14 12:49:40 +0530

    latest update

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -7,7 +7,8 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-if [ -d $SOURCE_DIRECTORY ] then
+if [ -d $SOURCE_DIRECTORY ] 
+then
     echo -e "$G source directory exists $N"
 else
     echo -e "$R source directory $SOURCE_DIRECTORY doesnot exists $N"
