d080128821f7d5274e598aa760f4161560cc8191
PavanKumarAnkam <pa1kumarankam@gmail.com>
2024-08-14 11:03:15 +0530

    latest update

---
+++ current-script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DATE='14-08-2024'
+MONTH='AUGUST'
+
+echo "b/f calling other script, date is:$DATE"
+echo "Process instance Id of current script is:$$"
+
+#./other-script.sh
+
+echo "a/f calling other script, date is:$DATE"
